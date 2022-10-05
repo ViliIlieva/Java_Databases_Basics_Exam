695fd487a9130dfcf41beb1b363b9913b038fb30
Velislava <100292906+ViliIlieva@users.noreply.github.com>
2022-10-05 11:57:52 +0300

    Update 05-09 Querying.sql

--- Database Basics MySQL Retake Exam - 31 March 2020/05-09 Querying.sql
+++ Database Basics MySQL Retake Exam - 31 March 2020/05-09 Querying.sql
@@ -1,1 +1,16 @@
 -- 05. Users
+SELECT username, gender, age
+FROM users
+ORDER BY age DESC, username ASC;
+---------------------------------------------------------------------------------------------------
+
+-- 06. Extract 5 most commented photos
+SELECT p.id, p.date as 'date_and_time', p.description,  count(p.id) as 'comentsCount'
+FROM photos as p
+JOIN comments as c on p.id = c.photo_id
+GROUP BY p.id
+ORDER BY comentsCount DESC, p.id asc
+LIMIT 5;
+---------------------------------------------------------------------------------------------------
+
+-- 07. Lucky users
