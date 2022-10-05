f737788ca8de17bddcb74b79a40992eb06b298c0
Velislava <100292906+ViliIlieva@users.noreply.github.com>
2022-10-05 13:46:05 +0300

    Update 05-09 Querying.sql

--- Database Basics MySQL Retake Exam - 31 March 2020/05-09 Querying.sql
+++ Database Basics MySQL Retake Exam - 31 March 2020/05-09 Querying.sql
@@ -14,3 +14,27 @@ LIMIT 5;
 ---------------------------------------------------------------------------------------------------
 
 -- 07. Lucky users
+SELECT concat(u.id, ' ', u.username) as id_username, u.email
+FROM users as u
+JOIN users_photos as p ON u.id = p.user_id
+WHERE p.photo_id = p.user_id
+ORDER BY p.user_id ASC;
+---------------------------------------------------------------------------------------------------
+
+-- 08. Count likes and comments
+SELECT p.id as 'photo_id', count(DISTINCT l.id) as 'likes_count', count(DISTINCT c.id) as 'coments_count'
+FROM photos as p
+LEFT JOIN likes as l on p.id = l.photo_id
+LEFT JOIN comments as c on p.id = c.photo_id
+GROUP BY p.id
+ORDER BY likes_count DESC, coments_count DESC, photo_id ASC;
+---------------------------------------------------------------------------------------------------
+
+-- 09. The photo on the tenth day of the month
+SELECT concat(left(`description`, 30), '...') as 'summary', `date`
+from photos
+WHERE day(`date`) = 10 
+ORDER BY `date` DESC;
+
+
+
