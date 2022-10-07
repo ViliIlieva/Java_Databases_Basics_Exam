b57c9fe2b117a0140a8677c7f146645661db5a7f
Velislava <100292906+ViliIlieva@users.noreply.github.com>
2022-10-07 09:52:00 +0300

    Update 05-09 Querying.sql

--- Database Basics MySQL Exam - 13 February 2022/05-09 Querying.sql
+++ Database Basics MySQL Exam - 13 February 2022/05-09 Querying.sql
@@ -15,3 +15,17 @@ FROM reviews
 WHERE content like 'My%' and length(content) > 61
 ORDER BY rating DESC;
 
+-- 08. First customers
+SELECT concat(c.first_name, ' ', c.last_name) as 'full_name', c.address, o.order_datetime
+FROM customers as c
+JOIN orders as o ON c.id = o.customer_id
+WHERE year(o.order_datetime) <= '2018'
+ORDER BY full_name DESC;
+
+-- 09. Best categories
+SELECT count(p.id) as 'items_count', c.name, sum(p.quantity_in_stock) as 'total_quantity'
+FROM products as p
+JOIN categories as c ON p.category_id = c.id
+GROUP BY c.id
+ORDER BY items_count DESC, total_quantity ASC
+LIMIT 5;
