118a986ccd5e56ffa6414419aa5e2cf665050895
Velislava <100292906+ViliIlieva@users.noreply.github.com>
2022-10-11 13:19:03 +0300

    Create 05-09 Querying.sql

---
+++ Database Basics MySQL Exam - 20 June 2021/05-09 Querying.sql
@@ -0,0 +1,30 @@
+-- 05. Cars
+SELECT c.make, c.model, c.condition
+FROM cars as c
+ORDER BY c.id;
+
+-- 06. Drivers and Cars
+SELECT d.first_name, d.last_name, c.make, c.model, c.mileage
+FROM drivers as d
+JOIN cars_drivers as cd on d.id = cd.driver_id
+JOIN cars as c on cd.car_id = c.id
+WHERE c.mileage is not null
+ORDER BY c.mileage DESC, d.first_name;
+
+-- 07. Number of courses
+SELECT c.id, c.make, c.mileage, count(cr.id) as 'count_of_courses', round(avg(cr.bill), 2) as 'count_of_courses'
+FROM cars as c
+left JOIN courses as cr on c.id = cr.car_id
+GROUP BY c.id
+HAVING count_of_courses <> 2
+ORDER BY count_of_courses DESC, c.id;
+
+-- 08. Regular clients
+SELECT c.full_name , count(cr.car_id) as 'count_of_cars', sum(cr.bill)
+from clients as c
+JOIN courses as cr on c.id = cr.client_id
+JOIN cars on cr.car_id = cars.id
+GROUP BY cr.client_id
+HAVING c.full_name LIKE '_a%' and count_of_cars > 1
+ORDER BY c.full_name;
+
